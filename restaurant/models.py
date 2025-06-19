bdf65e16c9195b54b4c8e38dbf0989eebdc26829
amine-meh <amine.mehouar@gmail.com>
2025-06-19 14:10:04 +0000

    Adding API End points for the Menu models and for the Booking model

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Booking(models.Model):
   name = models.CharField(max_length=255)
-  no_of_guests = models.SmallIntegerField(max_length=6)
+  no_of_guests = models.SmallIntegerField()
   bookingDate = models.DateTimeField()
 
   def __str__(self):
@@ -13,7 +13,7 @@ class Booking(models.Model):
 class Menu(models.Model):
   title = models.CharField(max_length=255)
   price = models.DecimalField(max_digits=10, decimal_places=2)
-  inventory = models.SmallIntegerField(max_length=5)
+  inventory = models.SmallIntegerField()
 
   def __str__(self):
     return self.title
