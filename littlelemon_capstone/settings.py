07979cd43d62b9569f17ae71f73e77677530f224
amine-meh <amine.mehouar@gmail.com>
2025-06-19 13:04:12 +0000

    Adding two models Booking and Menu

--- littlelemon_capstone/settings.py
+++ littlelemon_capstone/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'littlelemon_capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstone',
+        'USER' : 'root',
+        'PASSWORD' : 'root',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3308',
+        'OPTIONS': {
+             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
