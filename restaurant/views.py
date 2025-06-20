9444c82e6fa25606d7c9fb0a48c6a41aba96613c
amine-meh <amine.mehouar@gmail.com>
2025-06-20 14:27:53 +0000

    Adding Token Based Authentication for securing some API End Points

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,16 +3,20 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
+  permission_classes = [IsAuthenticated]
   queryset = Menu.objects.all()
   serializer_class = MenuSerializer
 
+
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [IsAuthenticated]
   queryset = Menu.objects.all()
   serializer_class = MenuSerializer
 
